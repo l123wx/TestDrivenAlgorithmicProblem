6a9986ea9df772e9b73186be4f84a346f9173dbf
廖文禧 <1219585136@qq.com>
2023-01-18 15:36:23 +0800

    test(FizzBuzz): add test

--- problems/FizzBuzz/index.test.ts
+++ problems/FizzBuzz/index.test.ts
@@ -1,1 +1,6 @@
-it('noting', () => { })
+import { handleSingleDigit } from './index'
+
+it('should return origin digit given input is not match any rule', () => {
+    expect(handleSingleDigit(1)).toBe('1')
+    expect(handleSingleDigit(2)).toBe('2')
+})
