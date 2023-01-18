ff825ab750063bbe099565ee8d43725487241e59
廖文禧 <1219585136@qq.com>
2023-01-18 15:57:26 +0800

    refactor(FizzBuzz): 消除过程依赖

--- problems/FizzBuzz/index.ts
+++ problems/FizzBuzz/index.ts
@@ -5,26 +5,24 @@ export const fizzBuzzGame = (args: number[]) => {
 }
 
 export const handleSingleDigit = (digit: number): string => {
+    let result = ''
     if (digit % 3 === 0 && digit % 5 === 0 && digit % 7 === 0) {
-        return 'FizzBuzzWhizz'
+        result = 'FizzBuzzWhizz'
+    } else if (digit % 3 === 0 && digit % 5 === 0) {
+        result = 'FizzBuzz'
+    } else if (digit % 3 === 0 && digit % 7 === 0) {
+        result = 'FizzWhizz'
+    } else if (digit % 5 === 0 && digit % 7 === 0) {
+        result = 'BuzzWhizz'
+    } else if (digit % 3 === 0) {
+        result = 'Fizz'
+    } else if (digit % 5 === 0) {
+        result = 'Buzz'
+    } else if (digit % 7 === 0) {
+        result = 'Whizz'
     }
-    if (digit % 3 === 0 && digit % 5 === 0) {
-        return 'FizzBuzz'
+    if (result === '') {
+        result = String(digit)
     }
-    if (digit % 3 === 0 && digit % 7 === 0) {
-        return 'FizzWhizz'
-    }
-    if (digit % 5 === 0 && digit % 7 === 0) {
-        return 'BuzzWhizz'
-    }
-    if (digit % 3 === 0) {
-        return 'Fizz'
-    }
-    if (digit % 5 === 0) {
-        return 'Buzz'
-    }
-    if (digit % 7 === 0) {
-        return 'Whizz'
-    }
-    return String(digit)
+    return result
 }
