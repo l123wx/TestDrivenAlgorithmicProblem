a226f05f6b507c20cd3797f46c6d7bef83d86606
廖文禧 <1219585136@qq.com>
2023-01-18 16:00:37 +0800

    refactor(FizzBuzz): 抽取函数，划定重构边界

--- problems/FizzBuzz/index.ts
+++ problems/FizzBuzz/index.ts
@@ -5,6 +5,14 @@ export const fizzBuzzGame = (args: number[]) => {
 }
 
 export const handleSingleDigit = (digit: number): string => {
+    let result = handleMod357(digit)
+    if (result === '') {
+        result = String(digit)
+    }
+    return result
+}
+
+const handleMod357 = (digit: number): string => {
     let result = ''
     if (digit % 3 === 0 && digit % 5 === 0 && digit % 7 === 0) {
         result = 'FizzBuzzWhizz'
@@ -20,9 +28,6 @@ export const handleSingleDigit = (digit: number): string => {
         result = 'Buzz'
     } else if (digit % 7 === 0) {
         result = 'Whizz'
-    }
-    if (result === '') {
-        result = String(digit)
     }
     return result
 }
