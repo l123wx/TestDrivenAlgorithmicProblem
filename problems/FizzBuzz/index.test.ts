77b3683ef7e31c0b67009657b4e8f99f1a5d31e1
廖文禧 <1219585136@qq.com>
2023-01-18 15:43:43 +0800

    test(FizzBuzz): add test

--- problems/FizzBuzz/index.test.ts
+++ problems/FizzBuzz/index.test.ts
@@ -15,3 +15,7 @@ it('should return Buzz given input can be divided by 5', () => {
 it('should return FizzBuzz given input can be divided by 3 and 5', () => {
     expect(handleSingleDigit(15)).toBe('FizzBuzz')
 })
+
+it('should return Whizz given input can be divided by 7', () => {
+    expect(handleSingleDigit(7)).toBe('Whizz')
+})
